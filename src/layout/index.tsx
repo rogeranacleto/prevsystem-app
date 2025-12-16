ae64c1d15bf76021fb533fa2f155abc587d5d297
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-12-16 01:32:35 -0300

    feat: change margin left

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,13 +1,22 @@
 import { Outlet } from "react-router"
-import { Sidebar } from "../components/Sidebar";
+import { Sidebar } from "../components/Sidebar"
 import { useState } from "react";
 export function Layout() {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   return (
     <div className="bg-[#080808] min-h-screen">
-      <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed}/>
-      <main className={`p-4 overflow-y-auto transition-all duration-300${isCollapsed ? "ml-20" : "ml-62"}`}>
+      <Sidebar
+        isCollapsed={isCollapsed}
+        setIsCollapsed={setIsCollapsed}
+      />
+
+      <main
+        className={`
+          p-4 overflow-y-auto transition-all duration-300
+          ${isCollapsed ? "ml-20" : "ml-64"}
+        `}
+      >
         <Outlet />
       </main>
     </div>
